808bc28de68d6dcd71dbfa70fbb1b4cff2fc0b47
Luiz Oliveira <luizcdev@gmail.com>
2020-02-20 23:49:39 -0300

    Criando controle de entregadores

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from '../config/multer';
 import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -6,12 +8,17 @@ import RecipientController from './app/controllers/RecipientController';
 import managerMiddleware from './app/middlewares/manager';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
+
+routes.put('/file', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 routes.get('/recipients/', RecipientController.index);
 routes.post('/recipients', managerMiddleware, RecipientController.store);
